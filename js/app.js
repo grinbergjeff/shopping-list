b0b998dbc4cdc601013327338f8ede9f518953bd
Jeffrey Grinberg <jeffgrinberg@Jeffrey-Grinbergs-MacBook.local>
2015-06-10 23:32:43 -0400

    outlined what I want in jQ

---
+++ js/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+	
+	/*Insert Enter Button to input new paragraph (item)*/
+	
+	/*Figure out how to add new items to list div*/
+	
+	/*Figure out how to add strikethrough/delete icons*/
+	
+	/*Incorporate full reset code*/
+	
+	/*Consider incorporating menu on side*/ 
+	
+	
+});
